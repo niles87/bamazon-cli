0323ef258ce793f4294cb1a6763d108966be3d62
niles87 <nbing19@gmail.com>
2020-02-16 07:22:30 -0800

    initial code for basic shopping app

---
+++ bamazonCustomer.js
@@ -0,0 +1,76 @@
+require("dotenv").config();
+var key = require("./key");
+var inquirer = require("inquirer");
+var colors = require("colors/safe");
+var mysql = require("mysql");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: key.keys.secret,
+  database: "bamazonDB",
+});
+
+colors.enable();
+
+connection.connect(err => {
+  if (err) throw err;
+  console.log(colors.yellow(`connected as id ${connection.threadId}`));
+  displayProductInfo();
+});
+
+var displayProductInfo = () => {
+  connection.query("SELECT * FROM products", (err, res) => {
+    if (err) throw err;
+    console.log(res);
+    addToShoppingCart();
+  });
+};
+
+var addToShoppingCart = () => {
+  inquirer
+    .prompt([
+      {
+        message: "Enter the ID of the product you'd like to add.",
+        name: "product-id",
+        validate: value => {
+          if (isNaN(value) === false) {
+            return true;
+          }
+          return false;
+        },
+      },
+      {
+        message: "Please enter the quantity desired.",
+        name: "quantity",
+        validate: value => {
+          if (isNaN(value) === false) {
+            return true;
+          }
+          return false;
+        },
+      },
+    ])
+    .then(answers => {
+      connection.query("SELECT * FROM products WHERE id=?", [answers["product-id"]], (err, res) => {
+        if (err) throw err;
+        var newStockQuantity = res[0].stock_quantity - answers.quantity;
+        var customerCost = res[0].price * answers.quantity;
+        if (answers.quantity <= res[0].stock_quantity) {
+          connection.query(
+            "UPDATE products SET ? WHERE ?",
+            [{ stock_quantity: newStockQuantity }, { id: answers["product-id"] }],
+            (err, res) => {
+              if (err) throw err;
+              console.log(colors.green.underline("\nAmount owed: $" + customerCost));
+              console.log(colors.green("\nThanks for your business!\n"));
+            }
+          );
+        } else {
+          console.log(colors.red("\nWe don't have enough in stock\n"));
+        }
+        connection.end();
+      });
+    });
+};
